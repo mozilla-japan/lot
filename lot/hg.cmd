7e0fa1b44fe56fe3d5a372f099243a0599d1228c
dynamis <dynamis@localhost>
2010-08-16 11:00:19 +0000

    lot: support for browserconfig.properties, hg.cmd update contributed by hATrayflood

--- lot/hg.cmd
+++ lot/hg.cmd
@@ -1,8 +1,12 @@
 @echo off
+hg.exe > nul 2>&1
+if "%ERRORLEVEL%" == "0" (
+hg.exe %*
+) else (
 for /f %%i in ("hg") DO set HGSCRIPT="%%~$PATH:i"
-if %HGSCRIPT% == "" (
+if "%HGSCRIPT%" == "" (
         echo Cannot find hg on PATH
         exit /b 1
 )
 python %HGSCRIPT% %*
-
+)
