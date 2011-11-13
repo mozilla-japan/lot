978009ee56398e37f67b88cd6b08f0efe6fe57e0
hATrayflood <hATrayflood@localhost>
2011-11-13 16:26:56 +0900

    lotcmd.bat: add JAVA_HOME. and fix indent style.

--- lotcmd.bat
+++ lotcmd.bat
@@ -6,16 +6,36 @@ set PATH=%PATH%;%~d0\mozilla-build\python
 set PATH=%PATH%;%~d0\mozilla-build\msys\bin
 set PATH=%PATH%;%~d0\git\bin
 set PATH=%PATH%;%~d0\ant\bin
-if "%SSH_AGENT_PID%" == "" (
-set HOME=%HOMEDRIVE%%HOMEPATH%
-for /f "eol=; tokens=1,2 delims==;" %%1 in ('ssh-agent.exe') do (
- if "%%1" == "SSH_AUTH_SOCK" set SSH_AUTH_SOCK=%%2
- if "%%1" == "SSH_AGENT_PID" set SSH_AGENT_PID=%%2
+
+set JDKROOTKEY=HKLM\SOFTWARE\JavaSoft\Java Development Kit
+set JDKROOTQRY=reg query "%JDKROOTKEY%" /v "CurrentVersion"
+%JDKROOTQRY% >nul 2>nul
+if %ERRORLEVEL% equ 0 (
+	for /f "tokens=2* delims=	 " %%A in ('%JDKROOTQRY%') do (
+		set JDKCURVER=%%B
+	)
 )
+set JDKCURVERKEY=%JDKROOTKEY%\%JDKCURVER%
+set JDKCURVERQRY=reg query "%JDKCURVERKEY%" /v "JavaHome"
+%JDKCURVERQRY% >nul 2>nul
+if %ERRORLEVEL% equ 0 (
+	for /f "tokens=2* delims=	 " %%A in ('%JDKCURVERQRY%') do (
+		set JAVA_HOME=%%B
+	)
+)
+
+if "%SSH_AGENT_PID%" == "" (
+	set HOME=%HOMEDRIVE%%HOMEPATH%
+	for /f "eol=; tokens=1,2 delims==;" %%1 in ('ssh-agent') do (
+		if "%%1" == "SSH_AUTH_SOCK" set SSH_AUTH_SOCK=%%2
+		if "%%1" == "SSH_AGENT_PID" set SSH_AGENT_PID=%%2
+	)
 )
 ssh-add
+
 start /b /wait
+
 if not "%SSH_AGENT_PID%" == "" (
-ssh-agent -k
+	ssh-agent -k
 )
 @endlocal
