e5229ee9b150351cf6e73a66aabd9242bf46a4f7
hATrayflood <h.rayflood@gmail.com>
2020-02-12 21:55:14 +0900

    improve l10n bot shell.

--- slackbot/slackbot.sh
+++ slackbot/slackbot.sh
@@ -1,11 +1,20 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-CURDIR=$(cd $(dirname ${0}) && pwd)
+CURDIR=$(cd $(dirname ${0})/.. && pwd)
 PYTHON_PATH=$(cd $(dirname ${PYTHON_EXE}) && pwd)
 
-(cd l10n/trunk/en-US && hg pull)
-(cd l10n/trunk/en-US && hg update -C)
-(cd src/trunk && git pull origin master)
+if [ ! -d ${CURDIR}/l10n/trunk/en-US ] ; then
+    mkdir -p ${CURDIR}/l10n/trunk/en-US
+    hg clone https://hg.mozilla.org/l10n/gecko-strings/ ${CURDIR}/l10n/trunk/en-US
+fi
+if [ ! -d ${CURDIR}/src/trunk ] ; then
+    mkdir -p ${CURDIR}/src/trunk
+    git clone https://github.com/mozilla-japan/gecko-l10n.git ${CURDIR}/src/trunk
+fi
 
-${PYTHON_PATH}/compare-locales --json /tmp/compare-locales.json l10n/trunk/en-US/_configs/browser.toml src/trunk ja
-${PYTHON_EXE} ${CURDIR}/slackbot.py
+(cd ${CURDIR}/l10n/trunk/en-US && hg pull)
+(cd ${CURDIR}/l10n/trunk/en-US && hg update -C)
+(cd ${CURDIR}/src/trunk && git pull origin master)
+
+${PYTHON_PATH}/compare-locales --json /tmp/compare-locales.json ${CURDIR}/l10n/trunk/en-US/_configs/browser.toml ${CURDIR}/src/trunk ja > /dev/null
+${PYTHON_EXE} ${CURDIR}/slackbot/slackbot.py
