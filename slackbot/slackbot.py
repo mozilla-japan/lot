6953cd1f4b506501ed269155d4c21ff303c26bf0
hATrayflood <h.rayflood@gmail.com>
2020-04-17 23:06:44 +0900

    change calendar check to pontoon.

--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -6,58 +6,19 @@ import traceback
 import json
 
 # pip install
-import ics
 import requests
+import pyquery.pyquery
 
-REKEASE_WIKI = "https://wiki.mozilla.org/Release_Management/Calendar"
-CALENDAR_PAGE = "https://www.google.com/calendar/embed?src=bW96aWxsYS5jb21fZGJxODRhbnI5aTh0Y25taGFiYXRzdHY1Y29AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
-ICS_URL = "https://calendar.google.com/calendar/ical/mozilla.com_dbq84anr9i8tcnmhabatstv5co%40group.calendar.google.com/public/basic.ics"
+PONTOON_URL = "https://pontoon.mozilla.org/ja/firefox/"
 NOW = datetime.datetime.utcnow()
 NOW_STR = NOW.isoformat()
 
 
-def getLocaleCheck(event):
-    return NOW_STR < str(event.begin) and "Final locale check" in event.name
-
-def getSignoff(event):
-    return NOW_STR < str(event.begin) and ("pre-release signoff" in event.name or "pre-release sign-off" in event.name)
-
-def getReleased(event):
-    return NOW_STR < str(event.begin) and "released" in event.name and not "RC" in event.name
-
-def formatLimit(event):
-    remain = datetime.datetime.fromisoformat(str(event.begin)).date() - NOW.date()
-    return "%s まであと *%d 日*" % (event.name, remain.days)
-
 def calendarCheck():
-    cal = ics.Calendar(requests.get(ICS_URL).text)
-    localeCheck = sorted(filter(getLocaleCheck, cal.events))
-    signoff     = sorted(filter(getSignoff,     cal.events))
-    released    = sorted(filter(getReleased,    cal.events))
-
-    error = False
-    text = ""
-    if len(localeCheck):
-        text += formatLimit(localeCheck[0]) + "\n"
-    else:
-        error = True
-        text += "チェック日不明\n"
-
-    if len(signoff):
-        text += formatLimit(signoff[0]) + "\n"
-    else:
-        error = True
-        text += "サインオフ期限不明\n"
-
-    if len(released):
-        text += formatLimit(released[0]) + "\n"
-    else:
-        error = True
-        text += "リリース日不明\n"
-
-    if error:
-        text += "カレンダー %s\n" % (CALENDAR_PAGE)
-    return text
+    d = pyquery.pyquery.PyQuery(url=PONTOON_URL)
+    deadline = datetime.datetime.strptime(d("#heading li.deadline time").text(), "%b %d, %Y")
+    remain = deadline.date() - NOW.date()
+    return "次回締切まであと *%d 日*\n" % (remain.days)
 
 
 def l10nStatus():
